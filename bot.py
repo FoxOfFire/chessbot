ecc0bfbe34320b364de85de202cbecb59f66ed1d
Balázs Vince <54199935+FoxOfFire@users.noreply.github.com>
2025-11-21 19:40:11 +0100

    rename

--- bot.py
+++ bot.py
@@ -3,91 +3,82 @@ from typing import Dict, List, Tuple
 
 import chess
 
-board = chess.Board()
 
-pieceVals: Dict[chess.PieceType, int] = {
-    chess.PAWN: 1000,
-    chess.KNIGHT: 3000,
-    chess.BISHOP: 3100,
-    chess.ROOK: 5000,
-    chess.QUEEN: 9000,
-    chess.KING: 10000000,
-}
-CHECKMOD = 500
-TRADEMOD = 200
+class Bot:
 
+    pieceVals: Dict[chess.PieceType, int] = {
+        chess.PAWN: 1000,
+        chess.KNIGHT: 3000,
+        chess.BISHOP: 3100,
+        chess.ROOK: 5000,
+        chess.QUEEN: 9000,
+        chess.KING: 10000000,
+    }
+    CHECKMOD = 500
+    TRADEMOD = 200
 
-def getval(board: chess.Board) -> int:
+    def getval(self, board: chess.Board) -> int:
 
-    val: int = 0
-    for i in chess.SQUARES:
-        piece = board.piece_at(i)
-        if piece is None:
-            continue
-        frend = 1 if board.turn == piece.color else -1
-        val += frend * pieceVals[piece.piece_type]
+        val: int = 0
+        for i in chess.SQUARES:
+            piece = board.piece_at(i)
+            if piece is None:
+                continue
+            frend = 1 if board.turn == piece.color else -1
+            val += frend * self.pieceVals[piece.piece_type]
 
-    return val
+        return val
 
+    def prev_mov(self, board: chess.Board) -> List[str]:
+        return [str(s) for s in board.move_stack]
 
-def prev_mov(board: chess.Board) -> List[str]:
-    return [str(s) for s in board.move_stack]
+    def minimax(
+        self,
+        board: chess.Board,
+        depth: int,
+    ) -> Tuple[int, List[chess.Move]]:
+        best: int = -10000000 if board.turn else 10000000
+        legal_moves = board.generate_legal_moves()
+        move: List[chess.Move] = []
 
+        for current_move in legal_moves:
+            mod = 0
+            if board.is_capture(current_move):
+                mod += self.TRADEMOD
+            if board.is_into_check(current_move):
+                mod -= self.CHECKMOD * 2
 
-def minimax(board: chess.Board, depth: int) -> Tuple[int, List[chess.Move]]:
-    best: int = -10000000 if board.turn else 10000000
-    legal_moves = board.generate_legal_moves()
-    move: List[chess.Move] = []
+            board.push(current_move)
+            if board.is_check():
+                mod += self.CHECKMOD
 
-    for current_move in legal_moves:
-        mod = 0
-        if board.is_capture(current_move):
-            mod += TRADEMOD
-        if board.is_into_check(current_move):
-            mod -= CHECKMOD * 2
+            if depth < 1:
+                val = self.getval(board)
+            else:
+                val = self.minimax(board, depth - 1)[0]
 
-        board.push(current_move)
-        if board.is_check():
-            mod += CHECKMOD
+            board.pop()
 
-        if depth < 1:
-            val = getval(board)
-        else:
-            val = minimax(board, depth - 1)[0]
+            if board.is_checkmate():
+                return -10000000, [current_move]
+            if board.is_stalemate():
+                return 0, [current_move]
 
-        board.pop()
+            val += mod
+            val *= 1 if board.turn else -1
 
-        if board.is_checkmate():
-            return -10000000, [current_move]
-        if board.is_stalemate():
-            return 0, [current_move]
+            if best == val:
+                move.append(current_move)
+            elif best < val and board.turn or best > val and not board.turn:
+                move = [current_move]
+                best = val
 
-        val += mod
-        val *= 1 if board.turn else -1
+            # print(prev_mov(board))
 
-        if best == val:
-            move.append(current_move)
-        elif best < val and board.turn or best > val and not board.turn:
-            move = [current_move]
-            best = val
+        return best, move
 
-        # print(prev_mov(board))
+    def randombot(self, board: chess.Board) -> chess.Board:
 
-    return best, move
-
-
-def randombot(board: chess.Board) -> chess.Board:
-
-    moves: List[chess.Move] = minimax(board, 2)[1]
-    board.push(choice(moves))
-    return board
-
-
-i: int = 0
-while not board.is_game_over() and not board.is_fifty_moves():
-
-    i += 1
-
-    board = randombot(board)
-    print()
-    print(board.unicode())
+        moves: List[chess.Move] = self.minimax(board, 2)[1]
+        board.push(choice(moves))
+        return board
